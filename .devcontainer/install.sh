7a764cea8f21773ef62391bb5298fd949ef0332d
cecilphillip <cecilphillip@gmail.com>
2020-04-18 13:10:59 +0000

    update install.sh with gh, zsh, and pip install

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,32 +3,22 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
-
-# install pyenv
-rm -rf ~/.pyenv
-
-sudo curl https://pyenv.run | bash
-
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-
-# reload bashrc
-source ~/.bashrc
-. ~/.bashrc
-
-# install Python 3.8.1 from pyenv
-pyenv install 3.8.1
-
+# create virtual environment
 python3 -m venv venv 
-# install flask
+source ./venv/bin/activate
 
-# install github hub
-wget https://github.com/github/hub/releases/download/v2.13.0/hub-linux-amd64-2.13.0.tgz
-tar -xf hub-linux-amd64-2.13.0.tgz
-sudo ./hub-linux-amd64-2.13.0/install
+# install requirements
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
-#clean up hub install
-rm -rf hub*
+# configure default shell
+chsh /bin/zsh
 
-git config --global hub.protocol https
+# install github cli
+gh_version=0.6.4
+gh_platform=linux_amd64
+
+wget https://github.com/cli/cli/releases/download/v${gh_version}/gh_${gh_version}_${gh_platform}.tar.gz
+tar -xf gh_${gh_version}_${gh_platform}.tar.gz
+sudo cp gh_${gh_version}_${gh_platform}/bin/gh /usr/local/bin/
+rm -rf gh_${gh_version}_linux*
